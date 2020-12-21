ec6e10a276545495cad7ce22acef780b1b6b409a
ozuit <tantd2@vng.com.vn>
2020-12-21 20:26:42 +0700

    Create main UI

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,38 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-flashcard'
-import 'react-flashcard/dist/index.css'
+import { FlashcardComponent } from 'react-flashcard'
+
+const cardData = [
+  {
+    front: {
+      text: "living outside, often in a tent",
+      image: "https://o.quizlet.com/RWRdgDus.uuqNDUrJ0ernA.jpg",
+    },
+    back: {
+      text: "Camping",
+    }
+  },
+  {
+    front: {
+      text: "the cultivation of plants",
+    },
+    back: {
+      text: "Gardening",
+    }
+  },
+  {
+    front: {
+      text: "make 'kite' then show it floating around",
+      image: "https://o.quizlet.com/DNSK53oa86VTpPMo18ov4A.jpg",
+    },
+    back: {
+      text: "Flying a kite",
+    }
+  },
+]
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <FlashcardComponent dataSource={cardData} onChange={(step, side) => console.log(step, side)} onSound={(text) => console.log(text)} />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,155 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState } from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const FlashcardComponent = ({ dataSource, onSound, onChange }) => {
+  const [step, setStep] = useState(1)
+  const [side, setSide] = useState("front")
+
+  const handleChangeSide = () => {
+    const newSide = side === "front" ? "back" : "front"
+    setSide(newSide)
+    onChange(step, newSide)
+  }
+
+  const handlePrev = () => {
+    const prevStep = step > 1 ? step - 1 : 1
+    setStep(prevStep)
+    onChange(prevStep, side)
+  }
+
+  const handleNext = () => {
+    const max = dataSource.length
+    const nextStep = step < max ? step + 1 : max
+    setStep(nextStep)
+    onChange(nextStep, side)
+  }
+
+  const handleSpeaker = () => {
+    const text = dataSource[step - 1][side].text
+    onSound(text)
+  }
+
+  return (
+    <div style={Styles.container}>
+      <div style={Styles.progress}>
+        <div style={Styles.bar}>
+          <span style={{ ...Styles.complete, width: `${step / dataSource.length * 100}%` }}></span>
+        </div>
+        <div style={Styles.number}>
+          {`${step}/${dataSource?.length}`}
+        </div>
+      </div>
+      <div style={Styles.card}>
+        <img style={Styles.soundButton} src="https://www.flaticon.com/svg/static/icons/svg/786/786272.svg" onClick={handleSpeaker} />
+        <div style={Styles.cardContent} onClick={handleChangeSide}>
+          {
+            side === "front" ? (
+              <div style={Styles.front}>
+                {
+                  dataSource[step - 1]?.front?.image && <img width="40%" height="40%" src={dataSource[step - 1]?.front?.image} />
+                }
+                <p>{dataSource[step - 1]?.front?.text}</p>
+              </div>
+            ) : (
+                <div style={Styles.back}>
+                  {
+                    dataSource[step - 1]?.back?.image && <img width="40%" height="40%" src={dataSource[step - 1]?.back?.image} />
+                  }
+                  <p>{dataSource[step - 1]?.back?.text}</p>
+                </div>
+              )
+          }
+        </div>
+      </div>
+      <div style={Styles.navigation}>
+        <div style={Styles.prevButton} onClick={handlePrev}>
+          <img width="100%" height="100%" src="https://www.flaticon.com/svg/static/icons/svg/318/318276.svg" />
+        </div>
+        <div style={Styles.nextButton} onClick={handleNext}>
+          <img width="100%" height="100%" src="https://www.flaticon.com/svg/static/icons/svg/467/467152.svg" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Styles = {
+  container: {
+    backgroundColor: "#f6f7fb",
+  },
+  progress: {
+    backgroundColor: "#ffffff",
+    height: 45,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  bar: {
+    display: "flex",
+    flex: 3,
+    backgroundColor: "#c5cfe8",
+    height: "0.75rem",
+    position: "relative",
+  },
+  complete: {
+    backgroundColor: "#4257b2",
+    bottom: 0,
+    display: "block",
+    height: "0.75rem",
+    left: 0,
+    maxWidth: "100%",
+    position: "absolute",
+    top: 0,
+    transition: "all .12s cubic-bezier(.47,0,.745,.715)",
+  },
+  number: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "flex-end",
+    fontSize: "0.625rem",
+    letterSpacing: "0.0625rem",
+    fontWeight: 600,
+  },
+  card: {
+    height: 400,
+    padding: 16,
+    position: "relative",
+  },
+  cardContent: {
+    backgroundColor: "#ffffff",
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    fontSize: "1.5rem",
+  },
+  soundButton: {
+    position: "absolute",
+    width: 25,
+    height: 25,
+    right: 26,
+    top: 26,
+  },
+  navigation: {
+    display: "flex",
+    justifyContent: "space-between",
+    paddingBottom: 16,
+  },
+  prevButton: {
+    width: 30,
+    height: 30,
+    marginLeft: 16,
+    backgroundColor: "#ffffff",
+    borderRadius: "50%",
+  },
+  nextButton: {
+    width: 30,
+    height: 30,
+    marginRight: 16,
+    backgroundColor: "#ffffff",
+    borderRadius: "50%",
+  },
 }
