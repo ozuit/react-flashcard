abc28ff9e5f1dfeef2a6fb8749c9df6b8124309a
ozuit <tantd2@vng.com.vn>
2020-12-21 23:43:08 +0700

    Updates

---
+++ precache-manifest.bc7fc86fd665ff18c4d317372bb3c8fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b5ab9528666cd155150177f2e27e9e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e33a66bdaca916ab3a9c",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "313a039f67be1665c9ff",
+    "url": "./static/js/2.7c96dbf1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7c96dbf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33a66bdaca916ab3a9c",
+    "url": "./static/js/main.2500737e.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.2500737e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b62b4dea98feb5d899f",
+    "url": "./static/js/runtime-main.5288f97c.js"
+  }
+]);
